a3949e566824c56b0c9de02aed75f7c9dd7f020d
janae445 <87738307+janae445@users.noreply.github.com>
2021-07-28 17:25:51 -0400

    Delete New_Bot.py

--- New_Bot.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-PATH = '/home/jcamargo09678/MOSTEC CS Project/chromedriver_linux64.zip/chromedriver'
-driver = webdriver.Chrome(PATH)
-driver.get('https://www.adidas.com/us/nmd_r1-shoes/GX0997.html?forceSelSize=GX0997_610')
