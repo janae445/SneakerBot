f695de909971acee5f608a4bd9026fa6f44922a9
janae445 <87738307+janae445@users.noreply.github.com>
2021-07-29 18:59:30 -0400

    Update main.py
    Final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webbrowser
+import time
 import scrapy
 from typing import ItemsView
 from scrapy.crawler import CrawlerProcess
@@ -10,6 +11,7 @@ rules = ( Rule(LinkExtractor(restrict_xpaths='//a[contains(@class,"product-link"
                 callback='singleProductParse'),
             Rule(LinkExtractor(restrict_xpaths='//li[@class="pagging-arrow right-arrow"]'),
                 follow=True))
+
 class AdidasSpider(CrawlSpider):
     name = "adidas"
     allowed_domains = ["adidas.com"]
@@ -20,13 +22,24 @@ class AdidasSpider(CrawlSpider):
         RawSize = ShoeSize + BaseSize
         ShoeSizeCode = int(RawSize)
         URL = 'https://www.adidas.com/us/' + str(type.replace(' ', '-')) + '/' + str(Model) + '.html?forceSelSize=' + str(Model) + '_' + str(ShoeSizeCode)
+        
+        total_breaks = 1
+        break_count = 0
+        print("This program started on " + time.ctime())
+        while(break_count < total_breaks):
+        time.sleep(3)
+        webbrowser.open(URL)
+        break_count = break_count + 1
         return URL
+      
     def new_func(self, Size):
         ShoeSize = (Size - 4) * 20
         return ShoeSize
+      
     def URLplain(self, Model, type):
         urlplain = 'https://www.adidas.com/us/'  + str(type.replace(' ', '-')) + '/' + str(Model) + '.html'
         return urlplain
+      
     def start_requests(self):
         Model = input('Model #: ')
         Size = int(input('Size: '))
@@ -38,6 +51,7 @@ class AdidasSpider(CrawlSpider):
         self.start_urls.append(URL)
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
+            
     def parse(self, response):
         price = response.css(".gl-price-item::text").get()
         print("SHOE PRICE =", price)
