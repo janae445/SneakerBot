4ad5fa037580636ebab049d7cef863aeb47005d3
janae445 <jcamargo09678@gmail.com>
2021-07-29 10:04:48 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   main.py  Final bot
     Changes not staged for commit:
    	modified:   practice.py
    	modified:   scrapy_checkstock.py
    	deleted:    sneakerbotmain.py
     Untracked files:
    	__pycache__/

---
+++ main.py
@@ -0,0 +1,53 @@
+import webbrowser
+import scrapy
+from typing import ItemsView
+from scrapy.crawler import CrawlerProcess
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+import time
+import webbrowser
+
+rules = ( Rule(LinkExtractor(restrict_xpaths='//a[contains(@class,"product-link")]',deny=('_[WM]\.html',)),
+                callback='singleProductParse'),
+            Rule(LinkExtractor(restrict_xpaths='//li[@class="pagging-arrow right-arrow"]'),
+                follow=True))
+class AdidasSpider(CrawlSpider):
+    name = "adidas"
+    allowed_domains = ["adidas.com"]
+    start_urls = []
+    def URLGenSize(self, Model, Size, type):
+        BaseSize = 530
+        ShoeSize = self.new_func(Size)
+        RawSize = ShoeSize + BaseSize
+        ShoeSizeCode = int(RawSize)
+        URL = 'https://www.adidas.com/us/' + str(type.replace(' ', '-')) + '/' + str(Model) + '.html?forceSelSize=' + str(Model) + '_' + str(ShoeSizeCode)
+        return URL
+    def new_func(self, Size):
+        ShoeSize = (Size - 4) * 20
+        return ShoeSize
+    def URLplain(self, Model, type):
+        urlplain = 'https://www.adidas.com/us/'  + str(type.replace(' ', '-')) + '/' + str(Model) + '.html'
+        return urlplain
+    def start_requests(self):
+        Model = input('Model #: ')
+        Size = int(input('Size: '))
+        type = input('Shoe Name: ')
+        URL = self.URLGenSize(Model, Size, type)
+        urlplain = self.URLplain(Model, type)
+        print(URL)
+        print(urlplain)
+        self.start_urls.append(URL)
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    def parse(self, response):
+        price = response.css(".gl-price-item::text").get()
+        print("SHOE PRICE =", price)
+
+def CheckStock():
+    process = CrawlerProcess()
+    process.crawl(AdidasSpider)
+    process.start()
+CheckStock()
+
+
